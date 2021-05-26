2ad61bc53499d6e07b72009f0f9f2e0a0bfd53dd
Billy Keating <wmontoothkeating@gmail.com>
2021-05-25 19:07:47 -0700

    Committing before pull

--- animal-adoption-database/server.js
+++ animal-adoption-database/server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const bodyParser = require("body-parser")
+const bodyParser = require("body-parser");
 
 // const routes = require("./routes");
 const app = express();
--- animal-adoption-database/src/App.js
+++ animal-adoption-database/src/App.js
@@ -2,11 +2,14 @@ import Search from './components/Search/Search';
 import Header from './components/Header/HeaderNav';
 import BodyNoLogin from './components/BodyNoLogin/BodyNoLogin';
 import Footer from './components/Footer/Footer';
+import API from './utils/API'
 import "./App.css";
 
 import 'bulma/css/bulma.min.css';
 
 // import './components/Search.css';
+
+API.sampleData().then(results => console.log(results))
 
 function App() {
   return (
