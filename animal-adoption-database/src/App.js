d05a63eac275f3afffd45d8d5f64d2875d6921c9
Manny Chacon <chmanuel2030@gmail.com>
2021-05-27 21:08:32 -0700

    commit before main branch pull

--- animal-adoption-database/src/App.js
+++ animal-adoption-database/src/App.js
@@ -1,23 +1,41 @@
+import React, { Component } from 'react';
+import axios from 'axios'
+import { Route, Link } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom';
+// compoonents
 import Search from './components/Search/Search';
 import Header from './components/Header/HeaderNav';
 import BodyNoLogin from './components/BodyNoLogin/BodyNoLogin';
 import Footer from './components/Footer/Footer';
+import Signup from './components/signup/sign-up'
+import LoginForm from './components/login-form/login-form'
 import "./App.css";
 import 'bulma/css/bulma.min.css';
 
+
+
 // import './components/Search.css';
 
-function App() {
-  return (
-    <div className="App container">
-      <header className="App-header">
-        <Header />
-        <Search />
-        <BodyNoLogin />
-        <Footer />
-      </header>
-    </div>
-  );
-}
+
+  function App() {
+    return (<BrowserRouter>
+      <div className="App container">
+        <header className="App-header">
+          <Header />
+          <Search />
+          <BodyNoLogin />
+          <Footer />
+        </header>
+        
+        <Route
+          path="/signup"
+          render={() =>
+            <Signup/>}
+        />
+     
+      </div>
+      </BrowserRouter>
+    );
+  }
 
 export default App;
--- animal-adoption-database/src/components/Header/HeaderNav.js
+++ animal-adoption-database/src/components/Header/HeaderNav.js
@@ -1,69 +1,116 @@
-import React from "react";
+import React, { Component } from 'react'
+import { Redirect } from 'react-router-dom'
+import { Route, Link } from 'react-router-dom'
+import '../../App.css';
+import axios from 'axios'
+import { BrowserRouter } from 'react-router-dom'
 
-function HeaderNav() {
-    return (
-        <nav class="navbar" role="navigation" aria-label="main navigation">
-            <div class="navbar-brand">
-                <a class="navbar-item" href="https://bulma.io">
-                    <img src="../../images/Animal_Database_Logo.png" width="112" height="28" alt="test" />
-                </a>
+class HeaderNav extends Component {
+    constructor() {
+        super()
+        this.logout = this.logout.bind(this)
+    }
 
-                <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample" href="test">
-                    <span aria-hidden="true"></span>
-                    <span aria-hidden="true"></span>
-                    <span aria-hidden="true"></span>
-                </a>
-            </div>
+    logout(event) {
+        event.preventDefault()
+        console.log('logging out')
+        axios.post('/user/logout').then(response => {
+            console.log(response.data)
+            if (response.status === 200) {
+                this.props.updateUser({
+                    loggedIn: false,
+                    username: null
+                })
+            }
+        }).catch(error => {
+            console.log('Logout error')
+        })
+    }
 
-            <div id="navbarBasicExample" class="navbar-menu">
-                <div class="navbar-start">
-                    <a class="navbar-item" href="test">
-                        Mewsletter
+    render() {
+        const loggedIn = this.props.loggedIn;
+        console.log('navbar render, props: ')
+        console.log(this.props);
+
+        return (
+            <nav class="navbar" role="navigation" aria-label="main navigation">
+                <div class="navbar-brand">
+                    <a class="navbar-item" href="https://bulma.io">
+                        <img src="../../images/Animal_Database_Logo.png" width="112" height="28" alt="Bulma.io" />
                     </a>
 
-                    <a class="navbar-item" href="test">
-                        Animal Tips
+                    <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample" href="test">
+                        <span aria-hidden="true"></span>
+                        <span aria-hidden="true"></span>
+                        <span aria-hidden="true"></span>
+                    </a>
+                </div>
+
+                <div id="navbarBasicExample" class="navbar-menu">
+                    <div class="navbar-start">
+                        <a class="navbar-item" href="test">
+                            Mewsletter
                     </a>
 
-                    <div class="navbar-item has-dropdown is-hoverable">
-                        <a class="navbar-link" href="test">
-                            More
+                        <a class="navbar-item" href="test">
+                            Animal Tips
+                    </a>
+
+                        <div class="navbar-item has-dropdown is-hoverable">
+                            <a class="navbar-link" href="test">
+                                More
                         </a>
 
-                        <div class="navbar-dropdown">
-                            <a class="navbar-item" href="test">
-                                About
+                            <div class="navbar-dropdown">
+                                <a class="navbar-item" href="test">
+                                    About
                             </a>
-                            <a class="navbar-item" href="test">
-                                Jobs & Volunteering
+                                <a class="navbar-item" href="test">
+                                    Jobs & Volunteering
                             </a>
-                            <a class="navbar-item" href="test">
-                                Contact Us
+                                <a class="navbar-item" href="test">
+                                    Contact Us
                             </a>
-                            <hr class="navbar-divider" />
-                            <a class="navbar-item" href="test">
-                                Report an issue
+                                <hr class="navbar-divider" />
+                                <a class="navbar-item" href="test">
+                                    Report an issue
                             </a>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="navbar-end">
+                        <div class="navbar-item">
+                            <div class="buttons">
+
+                                <BrowserRouter>
+
+                                    {loggedIn ? (
+                                        <section className="navbar-section">
+                                            <Link to="#" className="btn btn-link text-secondary" onClick={this.logout}>
+                                                <span className="text-secondary">Logout</span></Link>
+
+                                        </section>
+                                    ) : (
+                                        <section className="navbar-section">
+                                            <Link to="/login" className="btn btn-link text-secondary">
+                                                <span className="text-secondary">Login   </span>
+                                            </Link>
+
+                                            <Link to="/signup" className="btn btn-link">
+                                                <span className="text-secondary"> Sign up</span>
+                                            </Link>
+
+                                        </section>
+                                    )}
+                                </BrowserRouter>,
+                            </div>
                         </div>
                     </div>
                 </div>
+            </nav>
 
-                <div class="navbar-end">
-                    <div class="navbar-item">
-                        <div class="buttons">
-                            <a class="button is-primary" href="test">
-                                <strong>Sign up</strong>
-                            </a>
-                            <a class="button is-light" href="test">
-                                Log in
-                            </a>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </nav>
-
-    );
+        );
+    }
 }
-
 export default HeaderNav;
