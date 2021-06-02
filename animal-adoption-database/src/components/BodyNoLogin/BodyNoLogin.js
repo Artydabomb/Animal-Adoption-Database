7b08c5c8dea1afb025fd463eb1a6efd92c1bf7d4
Billy Keating <wmontoothkeating@gmail.com>
2021-06-02 16:41:58 -0700

    Animal info displays on screen

--- animal-adoption-database/src/components/BodyNoLogin/BodyNoLogin.js
+++ animal-adoption-database/src/components/BodyNoLogin/BodyNoLogin.js
@@ -13,18 +13,18 @@ function BodyNoLogin(props) {
             <div className="tile is-parent">
                 <div className="tile is-child box ">
                     <div className="columns">
-                        {Pup.map((dog, index) => (
-                            index < 4 ?
-                                < div className="column">
-                                    <CardContent
-                                        name={dog.data["242"].animalName}
-                                        age={dog.data["242"].animalAgeString}
-                                        breed={dog.data["242"].animalBreed}
-                                        location={dog.data["242"].animalLocationCitystate} />
-                                </div> : <>
-                                </>
+                        {searchResults.map(animal => (
+                            <div className="column">
+                                <CardContent 
+                                    name={animal.animalName}
+                                    age={animal.animalAgeString}
+                                    breed={animal.animalBreed}
+                                    location={animal.animalLocationCityState}
+                                    img={animal.animalThumbnailUrl}
+                                    description={animal.animalDescription.replace("<div class=\"rgDescription\">", "").replace("</div>", "")}
+                                />
+                            </div>
                         ))}
-
                     </div>
                     <div className="columns">
                         {Pup.map((dog, index) => (
--- animal-adoption-database/src/components/CardContent/CardContent.js
+++ animal-adoption-database/src/components/CardContent/CardContent.js
@@ -6,7 +6,7 @@ function CardContent(props) {
         <div className="petcard card ">
             <div className="card-image">
                 <figure className="image"></figure>
-                <img className="petimage" src="https://bulma.io/images/placeholders/128x128.png" alt="test" />
+                <img className="petimage" src={props.img} alt="test" />
             </div>
             <div className="card-content">
                 <div className="media">
@@ -24,11 +24,7 @@ function CardContent(props) {
                 </div>
 
                 <div className="content">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                    Phasellus nec iaculis mauris.
-                    <a href="test">#css</a> <a href="test">#responsive</a>
-                    <br />
-                    <time datetime="2016-1-1">11:09 PM - 1 Jan 2016</time>
+                    {props.description}
                 </div>
             </div>
         </div>
