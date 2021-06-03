2fe61c2dce3ca3ecca7d3bfe86a2c8f9ad9f2e0a
Maria Lara <74436232+maria-lara@users.noreply.github.com>
2021-06-02 20:04:56 -0700

    updated controller and saved animals route

---
+++ animal-adoption-database/server/routes/API/animals.js
@@ -0,0 +1,18 @@
+const router = require("express").Router();
+const animalsController = require("../../../controllers/animalsController");
+
+
+router
+  .route("/")
+  .get(animalsController.findAll)
+  .post(animalsController.create);
+
+
+router
+  .route("/:id")
+  .get(animalsController.findById)
+  .put(animalsController.update)
+  .delete(animalsController.remove);
+
+
+module.exports = router;
