a26a54904e182b337257e2686f62ea85543249f0
Billy Keating <wmontoothkeating@gmail.com>
2021-06-02 17:11:02 -0700

    Styling for cards, innerHTML set for descriptions, results increased from 10-12, dogs only (can be changed in rescuegroups.js routes)

--- animal-adoption-database/src/components/BodyNoLogin/BodyNoLogin.js
+++ animal-adoption-database/src/components/BodyNoLogin/BodyNoLogin.js
@@ -4,7 +4,6 @@ import "./BodyNoLogin.css";
 import Pup from "../../Pup.json";
 import SearchContext from "../../utils/SearchContext"
 
-
 function BodyNoLogin(props) {
     const { searchResults } = useContext(SearchContext);
     console.log(searchResults)
@@ -12,46 +11,19 @@ function BodyNoLogin(props) {
         <div className="tile is-ancestor parentTile">
             <div className="tile is-parent">
                 <div className="tile is-child box ">
-                    <div className="columns">
+                    <div className="columns is-multiline">
                         {searchResults.map(animal => (
-                            <div className="column">
+                            <div className="column is-one-quarter">
                                 <CardContent 
                                     name={animal.animalName}
                                     age={animal.animalAgeString}
                                     breed={animal.animalBreed}
-                                    location={animal.animalLocationCityState}
+                                    location={animal.animalLocationCitystate}
                                     img={animal.animalThumbnailUrl}
                                     description={animal.animalDescription.replace("<div class=\"rgDescription\">", "").replace("</div>", "")}
                                 />
                             </div>
                         ))}
-                    </div>
-                    <div className="columns">
-                        {Pup.map((dog, index) => (
-                            index >= 4 && index < 8 ?
-                                < div className="column">
-                                    <CardContent
-                                        name={dog.data["242"].animalName}
-                                        age={dog.data["242"].animalAgeString}
-                                        breed={dog.data["242"].animalBreed}
-                                        location={dog.data["242"].animalLocationCitystate} />
-                                </div> : <>
-                                </>
-                        ))}
-                    </div>
-                    <div className="columns">
-                        {Pup.map((dog, index) => (
-                            index >= 9 && index < 13 ?
-                                < div className="column">
-                                    <CardContent
-                                        name={dog.data["242"].animalName}
-                                        age={dog.data["242"].animalAgeString}
-                                        breed={dog.data["242"].animalBreed}
-                                        location={dog.data["242"].animalLocationCitystate} />
-                                </div> : <>
-                                </>
-                        ))}
-
                     </div>
                 </div>
             </div>
