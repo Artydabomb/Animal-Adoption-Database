78412f6f5ffbd62a4287969a9758fa02d7cddcda
Maria Lara <74436232+maria-lara@users.noreply.github.com>
2021-06-02 21:19:21 -0700

    updated controller to add to array

--- animal-adoption-database/server/routes/API/animals.js
+++ animal-adoption-database/server/routes/API/animals.js
@@ -1,12 +1,10 @@
 const router = require("express").Router();
 const animalsController = require("../../../controllers/animalsController");
 
-
 router
   .route("/")
   .get(animalsController.findAll)
   .post(animalsController.create);
-
 
 router
   .route("/:id")
