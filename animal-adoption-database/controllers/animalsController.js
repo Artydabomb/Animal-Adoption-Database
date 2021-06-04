9b4d8427fe66f04367b1c1f6bd3d7e1c489a51eb
Maria Lara <74436232+maria-lara@users.noreply.github.com>
2021-06-03 18:37:37 -0700

    updated to correct rescuegroups version

--- animal-adoption-database/controllers/animalsController.js
+++ animal-adoption-database/controllers/animalsController.js
@@ -26,7 +26,7 @@ module.exports = {
       .catch(err => res.status(422).json(err));
   },
   update: function(req, res) {
-    db.Users
+    db.User
       .findOneAndUpdate({ _id: req.params.id }, req.body)
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
