8e83a3433b47ebc0fc30e827684017c96932c0f2
Manny Chacon <chmanuel2030@gmail.com>
2021-06-05 09:04:45 -0700

    mewsletter deployed

--- animal-adoption-database/src/components/Mewsletter/Mewsletter.js
+++ animal-adoption-database/src/components/Mewsletter/Mewsletter.js
@@ -1,16 +1,19 @@
 import React from "react";
 import HeaderNav from "../Header/HeaderNav";
 import Footer from "../Footer/Footer"
+import "./Mewsletter.css"
 
 
 
 function Mewsletter() {
     return (
-        
+
         <div className="tile-is-ancestor">
             <HeaderNav />
-           <img src = "../images/Newsletter.png"></img>
-<Footer />
+            <div className = "mewsletter">
+                
+            </div>
+            <Footer />
         </div >
     );
 }
