c1ff09904dbb12e1f25b907abc95b4dbe7e22c6b
Manny Chacon <chmanuel2030@gmail.com>
2021-06-05 08:45:50 -0700

    mewsletter

---
+++ animal-adoption-database/src/components/Mewsletter/Mewsletter.js
@@ -0,0 +1,18 @@
+import React from "react";
+import HeaderNav from "../Header/HeaderNav";
+import Footer from "../Footer/Footer"
+
+
+
+function Mewsletter() {
+    return (
+        
+        <div className="tile-is-ancestor">
+            <HeaderNav />
+           <img src = "../images/Newsletter.png"></img>
+<Footer />
+        </div >
+    );
+}
+
+export default Mewsletter
