b204eb6198d786e5f38197ef77890a4925f6278d
Arthur Leung <arthur.leung3@gmail.com>
2021-06-05 10:57:14 -0500

    stuff

--- animal-adoption-database/src/components/SaveButton/SaveButton.js
+++ animal-adoption-database/src/components/SaveButton/SaveButton.js
@@ -3,20 +3,7 @@ import { useState } from "react";
 
 function Button() {
     const [noLogin, loggedIn] = useState('Unsaved');
-<<<<<<< HEAD
 
-    //Create function to send button information to API route
-
-    return (
-        <div>
-            <h1>{noLogin}</h1>
-            <button onClick={() => loggedIn('Saved!')}>Add to favorites!</button>
-            <button onClick={() => loggedIn('Unsaved')}>Removed from favorites</button>
-        </div>
-    );
-}
-=======
-   
     if (loggedIn) {
         return (
             <div className="has-text-centered">
@@ -25,7 +12,7 @@ function Button() {
                 <button className="button border is-danger is-light m-1" onClick={() => loggedIn('Unsaved')}>💔</button>
             </div>
         );
-}}
->>>>>>> 4377a7278a34b158ac354072d33c999f14c69982
+    }
+}
 
 export default Button;
