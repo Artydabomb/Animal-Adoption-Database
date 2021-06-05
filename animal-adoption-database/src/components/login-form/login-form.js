d6b9b58f6ca29a758f53304d5f95991444ae78d7
Adrian <74800016+acombs801@users.noreply.github.com>
2021-06-05 01:07:13 -0700

    finished styling for login/signup pages and searchbars

--- animal-adoption-database/src/components/login-form/login-form.js
+++ animal-adoption-database/src/components/login-form/login-form.js
@@ -30,7 +30,7 @@ class LoginForm extends Component {
     onButtonClick = () => {
         console.log("Button Clicked")
         this.props.reloadChild();
-      }
+    }
 
     handleSubmit(event) {
         event.preventDefault()
@@ -52,7 +52,7 @@ class LoginForm extends Component {
                     })
                     // update the state to redirect to home
                     this.setState({
-                        
+
                         redirectTo: '/'
                     })
                 }
@@ -70,49 +70,51 @@ class LoginForm extends Component {
             return (
                 <div>
                     <HeaderNav />
-                    <div class="login-form">
-                        <div class="login-title">
-                            <h4>Login</h4>
-                        </div>
-                    <form className="form-horizontal">
-                        <div className="form-group">
-                            <div className="col-1 col-ml-auto">
-                                <label className="form-label" htmlFor="username">Username:</label>
+                    <div className="login-form">
+                        <div className="container login-body">
+                            <div className="login-title">
+                                <h4>Login</h4>
                             </div>
-                            <div className="col-3 col-mr-auto">
-                                <input className="form-input"
-                                    type="text"
-                                    id="username"
-                                    name="username"
-                                    placeholder="Username"
-                                    value={this.state.username}
-                                    onChange={this.handleChange}
-                                />
-                            </div>
+                            <form className="form-horizontal">
+                                <div className="form-group">
+                                    <div className="col-1 col-ml-auto">
+                                        <label className="form-label" htmlFor="username">Username:</label>
+                                    </div>
+                                    <div className="col-3 col-mr-auto">
+                                        <input className="form-input"
+                                            type="text"
+                                            id="username"
+                                            name="username"
+                                            placeholder="Username"
+                                            value={this.state.username}
+                                            onChange={this.handleChange}
+                                        />
+                                    </div>
+                                </div>
+                                <div className="form-group">
+                                    <div className="col-1 col-ml-auto">
+                                        <label className="form-label" htmlFor="password">Password: </label>
+                                    </div>
+                                    <div className="col-3 col-mr-auto">
+                                        <input className="form-input"
+                                            placeholder="password"
+                                            type="password"
+                                            name="password"
+                                            value={this.state.password}
+                                            onChange={this.handleChange}
+                                        />
+                                    </div>
+                                </div>
+                                <div className="form-group ">
+                                    <div className="col-7"></div>
+                                    <button
+                                        className="btn btn-primary col-1 col-mr-auto login-btn"
+                                        onClick={this.handleSubmit}
+
+                                        type="submit">Login</button>
+                                </div>
+                            </form>
                         </div>
-                        <div className="form-group">
-                            <div className="col-1 col-ml-auto">
-                                <label className="form-label" htmlFor="password">Password: </label>
-                            </div>
-                            <div className="col-3 col-mr-auto">
-                                <input className="form-input"
-                                    placeholder="password"
-                                    type="password"
-                                    name="password"
-                                    value={this.state.password}
-                                    onChange={this.handleChange}
-                                />
-                            </div>
-                        </div>
-                        <div className="form-group ">
-                            <div className="col-7"></div>
-                            <button
-                                className="btn btn-primary col-1 col-mr-auto login-btn"
-                                onClick={this.handleSubmit}
-                                
-                                type="submit">Login</button>
-                        </div>
-                    </form>
                     </div>
                     <Footer />
                 </div>
--- animal-adoption-database/src/components/signup/sign-up.js
+++ animal-adoption-database/src/components/signup/sign-up.js
@@ -61,61 +61,63 @@ class Signup extends Component {
 				<div>
 					<HeaderNav />
 					<div className="signup-form">
-						<div class="signup-title">
-							<h4>Sign up</h4>	
+						<div className="signup-body">
+							<div class="signup-title">
+								<h4>Sign up</h4>
+							</div>
+							<form className="form-horizontal">
+								<div className="form-group">
+									<div className="col-1 col-ml-auto">
+										<label className="form-label" htmlFor="username">Username</label>
+									</div>
+									<div className="col-3 col-mr-auto">
+										<input className="form-input"
+											type="text"
+											id="username"
+											name="username"
+											placeholder="Username"
+											value={this.state.username}
+											onChange={this.handleChange}
+										/>
+									</div>
+								</div>
+								<div className="form-group">
+									<div className="col-1 col-ml-auto">
+										<label className="form-label" htmlFor="email">Email: </label>
+									</div>
+									<div className="col-3 col-mr-auto">
+										<input className="form-input"
+											type="email"
+											name="email"
+											value={this.state.email}
+											onChange={this.handleChange}
+										/>
+									</div>
+								</div>
+								<div className="form-group">
+									<div className="col-1 col-ml-auto">
+										<label className="form-label" htmlFor="password">Password: </label>
+									</div>
+									<div className="col-3 col-mr-auto">
+										<input className="form-input"
+											placeholder="password"
+											type="password"
+											name="password"
+											value={this.state.password}
+											onChange={this.handleChange}
+										/>
+									</div>
+								</div>
+								<div className="form-group ">
+									<div className="col-7"></div>
+									<button
+										className="btn btn-primary col-1 col-mr-auto signup-btn"
+										onClick={this.handleSubmit}
+										type="submit"
+									>Sign up</button>
+								</div>
+							</form>
 						</div>
-						<form className="form-horizontal">
-							<div className="form-group">
-								<div className="col-1 col-ml-auto">
-									<label className="form-label" htmlFor="username">Username</label>
-								</div>
-								<div className="col-3 col-mr-auto">
-									<input className="form-input"
-										type="text"
-										id="username"
-										name="username"
-										placeholder="Username"
-										value={this.state.username}
-										onChange={this.handleChange}
-									/>
-								</div>
-							</div>
-							<div className="form-group">
-								<div className="col-1 col-ml-auto">
-									<label className="form-label" htmlFor="email">Email: </label>
-								</div>
-								<div className="col-3 col-mr-auto">
-									<input className="form-input"
-										type="email"
-										name="email"
-										value={this.state.email}
-										onChange={this.handleChange}
-									/>
-								</div>
-							</div>
-							<div className="form-group">
-								<div className="col-1 col-ml-auto">
-									<label className="form-label" htmlFor="password">Password: </label>
-								</div>
-								<div className="col-3 col-mr-auto">
-									<input className="form-input"
-										placeholder="password"
-										type="password"
-										name="password"
-										value={this.state.password}
-										onChange={this.handleChange}
-									/>
-								</div>
-							</div>
-							<div className="form-group ">
-								<div className="col-7"></div>
-								<button
-									className="btn btn-primary col-1 col-mr-auto"
-									onClick={this.handleSubmit}
-									type="submit"
-								>Sign up</button>
-							</div>
-						</form>
 					</div>
 					<Footer />
 				</div>
@@ -124,4 +126,4 @@ class Signup extends Component {
 	}
 }
 
-	export default Signup
+export default Signup
