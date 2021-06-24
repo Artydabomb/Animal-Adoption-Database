072980cd3578e4341f7e053ff3bed2ed7089aae0
Billy Keating <wmontoothkeating@gmail.com>
2021-06-24 16:26:23 -0700

    Added content to the About page

--- client/src/components/About/About.js
+++ client/src/components/About/About.js
@@ -4,10 +4,22 @@ import "./About.css"
 
 function About() {
     return (
-        <div className="tile-is-ancestor">
-            
-            <Footer />
-        </div >
+        <>
+            <div className="tile-is-ancestor main">
+                <div className="tile is-parent">
+                    <div className="tile is-child box">
+                        <div className="columns is-centered">
+                            <div className="column is-four-fifths has-text-centered">
+                                <h1 className="title is-1 m-5">About Cosmopawlitan</h1>
+                                <p className="subtitle mt-5">Cosmopawlitan is a web application developed by five graduates from the UC Davis Full-Stack Web Development Bootcamp. Our goal is to create an online pet-adoption portal to help potential owners find a new companion, while also furthering our own familiarity with key web development tools such as React, NoSQL Databases, user authentication, Express servers, and more. To learn more about Cosmopawlitan or to contact the developers, visit this project's <a href="https://github.com/Artydabomb/Animal-Adoption-Database" target="_blank" rel="noopener noreferrer">GitHub page.</a></p>
+                                <br></br>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        <Footer />
+        </>
     );
 }
 
