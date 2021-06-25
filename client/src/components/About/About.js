4562969fb523100d8c66de536ffd80c2839a4161
Billy Keating <wmontoothkeating@gmail.com>
2021-06-24 19:39:30 -0700

    Started LargeCard component

--- client/src/components/About/About.js
+++ client/src/components/About/About.js
@@ -13,6 +13,8 @@ function About() {
                                 <h1 className="title is-1 m-5">About Cosmopawlitan</h1>
                                 <p className="subtitle mt-5">Cosmopawlitan is a web application developed by five graduates from the UC Davis Full-Stack Web Development Bootcamp. Our goal is to create an online pet-adoption portal to help potential owners find a new companion, while also furthering our own familiarity with key web development tools such as React, NoSQL Databases, user authentication, Express servers, and more. To learn more about Cosmopawlitan or to contact the developers, visit this project's <a href="https://github.com/Artydabomb/Animal-Adoption-Database" target="_blank" rel="noopener noreferrer">GitHub page.</a></p>
                                 <br></br>
+                                {/* <h3 className="title is-3 mb-5">Developers</h3> */}
+                                {/* Card Component will go here */}
                             </div>
                         </div>
                     </div>
---
+++ client/src/components/LargeCard/LargeCard.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./LargeCard.css"
+
+function LargeCard(props) {
+
+    return (
+        <div></div>
+    )
+}
+
+export default LargeCard
