9584ed4c430fb6a9aef5d27c9c0523d70beb3e04
Billy Keating <wmontoothkeating@gmail.com>
2021-06-26 14:13:31 -0700

    Clicking prev/next buttons now update the search context with a new page number

--- client/src/components/PrevNext/PrevNext.js
+++ client/src/components/PrevNext/PrevNext.js
@@ -1,12 +1,23 @@
-import React from "react";
+import React, {useContext, useState} from "react";
+import SearchContext from "../../utils/SearchContext";
 
 function PrevNext(props) {
+    const { page } = useContext(SearchContext);
+
+    function nextPage(){
+        props.setPage(page + 1)
+    }
+
+    function prevPage(){
+        props.setPage(page - 1)
+    }
+
     return(
         <div className="columns is-centered mt-5 m-3">
             <div className="column">
                 <nav className="pagination" role="navigation" aria-label="pagination">
-                    <a className="pagination-previous button">Previous Page</a>
-                    <a className="pagination-next button">Next Page</a>
+                    <a className="pagination-previous button" onClick={prevPage}>Previous Page</a>
+                    <a className="pagination-next button" onClick={nextPage}>Next Page</a>
                 </nav>
             </div>
         </div>
