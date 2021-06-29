0402fb834ace725886c8345aa929e9ef1adc7ead
Billy Keating <wmontoothkeating@gmail.com>
2021-06-29 15:24:46 -0700

    Finished front-end sex dropdown

--- client/src/components/Search/Search.js
+++ client/src/components/Search/Search.js
@@ -10,7 +10,7 @@ import { faSortDown } from '@fortawesome/free-solid-svg-icons'
 
 function Search(props) {
     const [formObject, setFormObject] = useState({searchField: ""})
-    const [advancedSearch, setadvancedSearch] = useState({size: "Size"})
+    const [advancedSearch, setadvancedSearch] = useState({size: "Size", activity: "Activity level", sex: "Sex"})
     const [advancedSearchHidden, setAdvancedSearchHidden] = useState("true")
     const { page, rows } = useContext(SearchContext);
 
@@ -47,13 +47,47 @@ function Search(props) {
     }
 
     function toggleSizeDropdown() {
-        document.getElementById("sizeDropdown").className === "dropdown is-active" ? document.getElementById("sizeDropdown").className = "dropdown" : document.getElementById("sizeDropdown").className = "dropdown is-active"
+        document.getElementById("sizeDropdown").className === "dropdown is-active m-1" ? document.getElementById("sizeDropdown").className = "dropdown m-1" : document.getElementById("sizeDropdown").className = "dropdown is-active m-1"
     }
 
     function selectSize(size) {
-        document.getElementById("sizeDropdown").className = "dropdown";
-        document.getElementById(size).className = "dropdown-item is-active"
+        document.getElementById("sizeDropdown").className = "dropdown m-1";
+        let sizes = document.getElementsByClassName("size");
+        for (const c of sizes) {
+            c.className="dropdown-item size"
+        }
+        document.getElementById(size).className = "dropdown-item size is-active"
         setadvancedSearch({...advancedSearch, size})
+    }
+
+    function toggleActivityDropdown() {
+        document.getElementById("activityDropdown").className === "dropdown m-1 is-active" ? document.getElementById("activityDropdown").className = "dropdown m-1" : document.getElementById("activityDropdown").className = "dropdown m-1 is-active"
+    }
+
+    function selectActivity(activity) {
+        // Turn off the dropdown
+        document.getElementById("activityDropdown").className = "dropdown m-1";
+        // Iterate over other activities to remove is-active
+        let activities = document.getElementsByClassName("activity");
+        for (const c of activities) {
+                c.className="dropdown-item activity"
+        }
+        document.getElementById(activity).className = "dropdown-item activity is-active"
+        setadvancedSearch({...advancedSearch, activity})
+    }
+
+    function toggleSexDropdown() {
+        document.getElementById("sexDropdown").className === "dropdown is-active m-1" ? document.getElementById("sexDropdown").className = "dropdown m-1" : document.getElementById("sexDropdown").className = "dropdown is-active m-1"
+    }
+
+    function selectSex(sex) {
+        document.getElementById("sexDropdown").className = "dropdown m-1";
+        let sexes = document.getElementsByClassName("sex");
+        for (const c of sexes) {
+            c.className="dropdown-item sex"
+        }
+        document.getElementById(sex).className = "dropdown-item sex is-active"
+        setadvancedSearch({...advancedSearch, sex})
     }
 
     const options = [
@@ -127,37 +161,85 @@ function Search(props) {
         <div hidden={advancedSearchHidden} >
             <div className="box mb-5 main">
                 <div className="columns">
-                    <div className="column is-one-fifth has-text-centered mt-2">
+                    <div className="column is-one-fifth has-text-centered mt-3">
                         <p className="title is-5">Advanced Search:</p>
                     </div>
-                    {/* Advanced Search - Size */}
-                    <div className="column">
-                        <div className="dropdown" id="sizeDropdown">
-                            <div className="dropdown-trigger">
-                                <button className="button" aria-haspopup="true" aria-controls="dropdown-menu" onClick={toggleSizeDropdown}>
-                                <span>{advancedSearch.size}</span>
-                                <span className="icon is-small">
-                                    <FontAwesomeIcon icon={faSortDown} />
-                                </span>
-                                </button>
-                            </div>
-                            <div className="dropdown-menu" id="dropdown-menu" role="menu">
-                                <div className="dropdown-content">
-                                <a className="dropdown-item" id="Small" onClick={() => selectSize("Small")}>
-                                    Small
-                                </a>
-                                <a className="dropdown-item" id="Medium" onClick={() => selectSize("Medium")}>
-                                    Medium
-                                </a>
-                                <a className="dropdown-item" id="Large" onClick={() => selectSize("Large")}>
-                                    Large
-                                </a>
-                                <a className="dropdown-item" id="X-Large" onClick={() => selectSize("X-Large")}>
-                                    X-Large
-                                </a>
+                    <div className="column is-four-fifths">
+                        {/* Advanced Search - Size */}
+                            <div className="dropdown m-1" id="sizeDropdown">
+                                <div className="dropdown-trigger">
+                                    <button className="button" aria-haspopup="true" aria-controls="dropdown-menu" onClick={toggleSizeDropdown}>
+                                    <span>{advancedSearch.size}</span>
+                                    <span className="icon is-small">
+                                        <FontAwesomeIcon icon={faSortDown} />
+                                    </span>
+                                    </button>
+                                </div>
+                                <div className="dropdown-menu" id="dropdown-menu" role="menu">
+                                    <div className="dropdown-content">
+                                    <a className="dropdown-item size" id="Small" onClick={() => selectSize("Small")}>
+                                        Small
+                                    </a>
+                                    <a className="dropdown-item size" id="Medium" onClick={() => selectSize("Medium")}>
+                                        Medium
+                                    </a>
+                                    <a className="dropdown-item size" id="Large" onClick={() => selectSize("Large")}>
+                                        Large
+                                    </a>
+                                    <a className="dropdown-item size" id="X-Large" onClick={() => selectSize("X-Large")}>
+                                        X-Large
+                                    </a>
+                                    </div>
                                 </div>
                             </div>
-                        </div>
+                        {/* Advanced Search - Activity level */}
+                            <div className="dropdown m-1" id="activityDropdown">
+                                <div className="dropdown-trigger">
+                                    <button className="button" aria-haspopup="true" aria-controls="dropdown-menu" onClick={toggleActivityDropdown}>
+                                    <span>{advancedSearch.activity}</span>
+                                    <span className="icon is-small">
+                                        <FontAwesomeIcon icon={faSortDown} />
+                                    </span>
+                                    </button>
+                                </div>
+                                <div className="dropdown-menu" id="dropdown-menu" role="menu">
+                                    <div className="dropdown-content">
+                                    <a className="dropdown-item activity" id="Not Active" onClick={() => selectActivity("Not Active")}>
+                                    Not Active
+                                    </a>
+                                    <a className="dropdown-item activity" id="Slightly Active" onClick={() => selectActivity("Slightly Active")}>
+                                    Slightly Active
+                                    </a>
+                                    <a className="dropdown-item activity" id="Moderately Active" onClick={() => selectActivity("Moderately Active")}>
+                                    Moderately Active
+                                    </a>
+                                    <a className="dropdown-item activity" id="Highly Active" onClick={() => selectActivity("Highly Active")}>
+                                    Highly Active
+                                    </a>
+                                    </div>
+                                </div>
+                            </div>
+                        {/* Advanced Search - Sex */}
+                        <div className="dropdown m-1" id="sexDropdown">
+                                <div className="dropdown-trigger">
+                                    <button className="button" aria-haspopup="true" aria-controls="dropdown-menu" onClick={toggleSexDropdown}>
+                                    <span>{advancedSearch.sex}</span>
+                                    <span className="icon is-small">
+                                        <FontAwesomeIcon icon={faSortDown} />
+                                    </span>
+                                    </button>
+                                </div>
+                                <div className="dropdown-menu" id="dropdown-menu" role="menu">
+                                    <div className="dropdown-content">
+                                    <a className="dropdown-item sex" id="Male" onClick={() => selectSex("Male")}>
+                                    Male
+                                    </a>
+                                    <a className="dropdown-item sex" id="Female" onClick={() => selectSex("Female")}>
+                                    Female
+                                    </a>
+                                    </div>
+                                </div>
+                            </div>
                     </div>
                 </div>
             </div>
