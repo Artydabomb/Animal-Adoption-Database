e1a054bbb56bf96f901f94b35bfe8255e026b1f3
Billy Keating <wmontoothkeating@gmail.com>
2021-06-29 15:51:54 -0700

    Back end rescuegroups api routes should be done, just need to connect to front end

--- server/routes/API/rescuegroups.js
+++ server/routes/API/rescuegroups.js
@@ -8,6 +8,60 @@ router.route("/").post(function(req, res) {
     console.log("Search term in back-end API :" + req.body.searchField);
     console.log("Species to search in back-end API: " + req.body.species)
     console.log("PAGE: " + req.body.page)
+    let filters=[
+        {
+        "fieldName" : "animalBreed",
+        "operation" : "contains",
+        "criteria" : req.body.searchField || " "
+        },
+        {
+        "fieldName" : "animalLocationDistance",
+        "operation" : "radius",
+        "criteria" : "40"
+        },
+        {
+        "fieldName" : "animalSpecies",
+        "operation" : "equals",
+        "criteria" : req.body.species || "dog"
+        },
+        {
+        "fieldName" : "animalLocation",
+        "operation" : "equals",
+        "criteria" : req.body.zipCode || "95616"
+        },
+        {
+        "fieldName" : "animalStatus",
+        "operation" : "equals",
+        "criteria" : "available"
+        }
+    ]
+    if (req.body.activity) {
+        filters.push(
+            {
+                "fieldName": "animalActivityLevel",
+                "operation": "equals",
+                "criteria": req.body.activity
+            }
+        )
+    }
+    if (req.body.size) {
+        filters.push(
+            {
+                "fieldName": "animalGeneralSizePotential",
+                "operation": "equals",
+                "criteria": req.body.size
+            }
+        )
+    }
+    if (req.body.sex) {
+        filters.push(
+            {
+                "fieldName": "animalSex",
+                "operation": "equals",
+                "criteria": req.body.sex
+            }
+        )
+    }
     return axios.post("https://api.rescuegroups.org/http/v2.json", {
         "apikey" : process.env.API_KEY,
         "objectType" : "animals",
@@ -18,41 +72,12 @@ router.route("/").post(function(req, res) {
             "resultSort" : "animalID",
             "resultOrder" : "asc",
             "calcFoundRows" : "Yes",
-            "filters" : [
-                {
-                "fieldName" : "animalBreed",
-                "operation" : "contains",
-                "criteria" : req.body.searchField || " "
-                },
-                {
-                "fieldName" : "animalLocationDistance",
-                "operation" : "radius",
-                "criteria" : "90"
-                },
-                {
-                "fieldName" : "animalSpecies",
-                "operation" : "equals",
-                "criteria" : req.body.species || "dog"
-                },
-                {
-                "fieldName" : "animalLocationDistance",
-                "operation" : "radius",
-                "criteria" : "30"
-                },
-                {
-                "fieldName" : "animalLocation",
-                "operation" : "equals",
-                "criteria" : req.body.zipCode || "95616"
-                },
-                {
-                "fieldName" : "animalStatus",
-                "operation" : "equals",
-                "criteria" : "available"
-                }
-            ],
+            "filters" : filters,
             "fields": ["animalID","animalAgeString", "animalGeneralAge", "animalBreed","animalDescriptionPlain","animalLocationCitystate","animalName","animalPrimaryBreed","animalSpecies","animalThumbnailUrl","animalUrl","animalPictures"]
         }
     }).then(response => {
+        console.log(response.config.data)
+        console.log(response.data.messages.generalMessages)
         res.json(response.data)
     }).catch(err => {
         console.log(err);
